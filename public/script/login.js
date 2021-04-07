aebb0b092ab6b13f7c7eb035556198e271032d8c
vlad <goroy007@gmail.com>
2021-04-07 22:26:34 +0800

    added firebase database and done mobile authentication login

--- public/script/login.js
+++ public/script/login.js
@@ -2,15 +2,16 @@
 
 
 
+
 // To apply the default browser preference instead of explicitly setting it.
 firebase.auth().useDeviceLanguage();
 
 
 
-
-
+window.onload = function(){
     render();
 
+}
 
 
 function render(){
@@ -28,28 +29,76 @@ function render(){
 
 }
 
-
-function phoneAuth(){
+function phoneAuth() {
     //get the number
-    let number = document.getElementById("number").value;
-    //Phone number authenticator, params: number , recapcha
-    firebase.auth.SignInWithPhoneNumber(number, window.recaptchaVerifier).then(function(confirmation){
-        window.confirmationResult = confirmationResult;
-        codeResult = confirmationResult;
-        console.log(codeResult);
+    var number=document.getElementById('number').value;
+    //phone number authentication function of firebase
+    //it takes two parameter first one is number,,,second one is recaptcha
+    firebase.auth().signInWithPhoneNumber(number,window.recaptchaVerifier).then(function (confirmationResult) {
+        //s is in lowercase
+        window.confirmationResult=confirmationResult;
+        coderesult=confirmationResult;
+        console.log(coderesult);
         alert("Message sent");
-    }).catch(function(error){
+    }).catch(function (error) {
         alert(error.message);
     });
 }
 
-function codeVerify(){
-    let code = document.getElementById("verification-code").value;
-    codeResult.confirm(code).then(function(result){
-        alert("successfully registered");
-        console.log(result.user);
-    }).catch(function(error){
+
+
+function codeverify() {
+    var code=document.getElementById('verificationCode').value;
+    coderesult.confirm(code).then(function (result) {
+        alert("Successfully registered");
+        let exists = checkUserExistence(document.getElementById("number").value);
+        console.log(`exists ${exists}`)
+
+        if(!exists){
+            makeNewUser(document.getElementById("number").value,"bobby");
+        }
+
+        var user=result.user;
+        console.log(user);
+
+    }).catch(function (error) {
         alert(error.message);
     });
-    
 }
+
+function checkUserExistence(phone){
+    let exists = false;
+    // var starCountRef = firebase.database().ref('users/');
+    // starCountRef.on('value', (snapshot) => {
+    // const data = snapshot.val();
+    // updateStarCount(postElement, data);
+    firebase.database().ref('users')
+    .equalTo(phone).once('value', data => {
+            data.forEach(() => {
+                // If it does, output an error
+                exists = true;
+            });
+
+});
+    return exists
+}
+
+/**
+ * Function that creates a new user in the firebase database
+ * @param {1} phone : the new users mobile number 
+ * @param {2} username: the new users username 
+ */
+function makeNewUser(phone,username){
+
+    firebase.database().ref(`users/${phone}`).set({
+        username: username
+      });
+}
+
+
+
+// let loginButton = document.getElementById("loginButton");
+// let sendButton = document.getElementById("send-button");
+
+// loginButton.onclick = codeverify();
+// sendButton.onclick = phoneAuth();
