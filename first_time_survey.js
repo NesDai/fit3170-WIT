2791a92bb3d761c4d0871fa7f048b439708ea17d
Morad <mabo0003@student.monash.edu>
2021-04-10 19:12:40 +0800

    Created greeting function
    This function will display the first message the user sees when they first sign up and prompts for the questions to begin

--- first_time_survey.js
+++ first_time_survey.js
@@ -1,7 +1,32 @@
 var messages = document.getElementById('messages');
 
 var questions = ["Question 2", "Question 3", "Question 4"];
-var curr_question = 2;
+var curr_question = 1;
+
+function greeting(){
+  let ques_template = '<div class="space">\
+                              <div class="message-container sender">\
+                                  <p>Hi! I am the chatbot for this App.</p>\
+                                  <p>To get started, I would like to get to know you better by asking a few questions.</p>\
+                                  <p>Question ' + curr_question + '</p>\
+                              </div>\
+                          </div>';
+      let opt_template =  '<div class="space">\
+                              <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" onclick="choose(this)">\
+                                  Option 1\
+                              </button>\
+                              <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" onclick="choose(this)">\
+                                  Option 2\
+                              </button>\
+                              <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" onclick="choose(this)">\
+                                  Option 3\
+                              </button>\
+                          </div>'
+      curr_question += 1;
+      messages.innerHTML += ques_template;
+      messages.innerHTML += opt_template;
+      $('#messages').animate({scrollTop: $('#messages').prop("scrollHeight")}, 1000);
+}
 
 function choose(button){
     let content = button.textContent.trim()
