e07ccb4d3ca7d468981a45867044cd0cdf34f794
Arissha Yasmin <ared0005@student.monash.edu>
2021-05-12 15:18:57 +0800

    implement dynamic button functionality
    add functionalities when add reply button is clicked

--- public/script/forum.js
+++ public/script/forum.js
@@ -4,5 +4,8 @@ function addReply(){
     document.getElementById("comment_reply_input").className="showTextInput"
 
     // show send button
-    // document.getElementById("send_reply_btn").className="showButton"
+    document.getElementById("send_reply_btn").className="showButton mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" 
+
+    // hide add reply button
+    document.getElementById("add_reply_btn").className="hideReply mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
 }
