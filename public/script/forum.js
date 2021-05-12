8ad26939d663f71d40d96f61945c9aec18895c66
Arissha Yasmin <ared0005@student.monash.edu>
2021-05-12 13:00:14 +0800

    add comment structure for a post

---
+++ public/script/forum.js
@@ -0,0 +1,8 @@
+function addReply(){
+
+    // show text input
+    document.getElementById("comment_reply_input").className="showTextInput"
+
+    // show send button
+    // document.getElementById("send_reply_btn").className="showButton"
+}
