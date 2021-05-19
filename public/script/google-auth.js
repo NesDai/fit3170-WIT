37025b85f4e79cd505b62f452e1cab41325ac1e1
Chia Yong Peng <45567239+randcyp@users.noreply.github.com>
2021-05-19 20:40:43 +0900

    Fix incorrect redirect logic when logging in.

--- public/script/google-auth.js
+++ public/script/google-auth.js
@@ -2,51 +2,23 @@
  * Function called when clicking the Login/Logout button.
  */
 function toggleSignIn() {
-    if (!firebase.auth().currentUser) {
-        var provider = new firebase.auth.GoogleAuthProvider();
-        firebase.auth().signInWithPopup(provider).then(function (result) {
-            // This gives you a Google Access Token. You can use it to access the Google API.
-            var token = result.credential.accessToken;
-            // The signed-in user info.
-            var user = result.user;
-        }).catch(function (error) {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            // The email of the user's account used.
-            var email = error.email;
-            // The firebase.auth.AuthCredential type that was used.
-            var credential = error.credential;
-            if (errorCode === 'auth/account-exists-with-different-credential') {
-                alert('You have already signed up with a different auth provider for that email.');
-                // If you are using multiple auth providers on your app you should handle linking
-                // the user's accounts here.
-            } else {
-                console.error(error);
-            }
-        });
-    } else {
-        firebase.auth().signOut();
-    }
-}
-
-/**
- * initApp handles setting up UI event listeners and registering Firebase auth listeners:
- *  - firebase.auth().onAuthStateChanged: This listener is called when the user is signed in or
- *    out, and that is where we update the UI.
- */
-function initApp() {
-    // Listening for auth state changes.
     firebase.auth().onAuthStateChanged(function (user) {
         if (user) {
             // User is signed in.
             alert("successfully logged in");
             window.location = "main_page.html";
+        } else {
+            // User is signed out
+            var provider = new firebase.auth.GoogleAuthProvider();
+            firebase.auth().signInWithPopup(provider).then(result => {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                var token = result.credential.accessToken;
+                // The signed-in user info.
+                var user = result.user;
+            }).catch(function (error) {
+                // Handle Errors here.
+                console.error(error);
+            });
         }
     });
-
 }
-
-window.onload = function () {
-    initApp();
-};
