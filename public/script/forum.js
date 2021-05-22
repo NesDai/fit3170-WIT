c2124913b0a2aea19e7c4dbfe4060887c3008a4f
Arissha Yasmin <ared0005@student.monash.edu>
2021-05-22 18:13:07 +0800

    remove addReply() function

--- public/script/forum.js
+++ public/script/forum.js
@@ -27,18 +27,6 @@ function hideTranslationModal(){
 }
 
 
-function addReply(){
-
-    // show text input
-    document.getElementById("comment_reply_input").className="showTextInput"
-
-    // show send button
-    document.getElementById("send_reply_btn").className="showButton mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
-
-    // hide add reply button
-    document.getElementById("add_reply_btn").className="hideReply mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
-}
-
 
 //Making a new post
 function makeNewPost() {
