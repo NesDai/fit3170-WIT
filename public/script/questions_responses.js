6902f5bdbc375d7544ff75eb9f3f503ed048d92d
Dan <dyue0003@student.monash.edu>
2021-05-25 11:23:02 +0800

    Added subquestions functionality in questions and responses

--- public/script/questions_responses.js
+++ public/script/questions_responses.js
@@ -1,5 +1,6 @@
 // Initialising variables
 let selected = null;
+let sub = false;
 let responsesList = document.getElementById("responses-list");
 let questionsList = document.getElementById("questions-list");
 
@@ -9,16 +10,23 @@ let responses = ["Response 1", "Response 2", "Response 3"];
 let questions = [{question_number: "1.1", question: "What is your name?", type: "none"},
                 {question_number: "1.2", question: "Where are you from?", type: "multiple-choice-sub-question"},
                 {question_number: "1.3", question: "What is your first name, last name, and middle name?", type: "none"}];
-let subquestions = ["Subquestion 1", "Subquestion 2", "Subquestion 3"];
+let subquestions = [{question_number: "1.2.1", question: "Subquestion: What is your name?", type: "none"},
+                    {question_number: "1.2.2", question: "Subquestion: Where are you from?", type: "none"}, 
+                    {question_number: "1.2.3", question: "Subquestion: What is your first name, last name, and middle name?", type: "none"}];
 
 /**
  * Adds list of questions in questions to the UI
  */
 function addQuestionsList() {
+    sub = false;
     let questionsListString = "";
     for (let i = 0; i < questions.length; i++) {
-    questionsListString += '<li class="mdl-list__item mdl-list__item--two-line" \
-                    onclick="changeQuestion(' + i + ')" id="q' + i + '" style="cursor: pointer;"> \
+        let innerFunction = "changeQuestion(" + i + ")";
+        if (questions[i].type == "multiple-choice-sub-question") {
+            innerFunction = "changeSubQuestion(" + i + ")";
+        }
+        questionsListString += '<li class="mdl-list__item mdl-list__item--two-line" \
+                    onclick="' + innerFunction + '" id="q' + i + '" style="cursor: pointer;"> \
                                 <span class="mdl-list__item-primary-content"> \
                                 <span>Question ' + questions[i].question_number + '</span> \
                                 <span class="mdl-list__item-sub-title"> \
@@ -45,7 +53,13 @@ function changeQuestion(index) {
     selected = document.getElementById(q)
     selected.style.opacity = 0.5;
 
-    let header = "<h3>Question " + (index+1) + " Responses</h3>"
+    let header = "";
+    if (!sub) {
+        header = "<h3>Question " + questions[index].question_number + " Responses</h3>"
+    }
+    else {
+        header = "<h3>Question " + subquestions[index].question_number + " Responses</h3>"
+    }
     responsesList.innerHTML = header;
 
     let responsesListString = "";
@@ -59,6 +73,30 @@ function changeQuestion(index) {
                                 </li>';
     }
     responsesList.innerHTML += responsesListString;
+}
+
+function changeSubQuestion() {
+    sub = true;
+    let subQuestionsListString = "";
+    for (let i = 0; i < subquestions.length; i++) {
+        let innerFunction = "changeQuestion(" + i + ")";
+        subQuestionsListString += '<li class="mdl-list__item mdl-list__item--two-line" \
+                    onclick="' + innerFunction + '" id="q' + i + '" style="cursor: pointer;"> \
+                                <span class="mdl-list__item-primary-content"> \
+                                <span>Question ' + subquestions[i].question_number + '</span> \
+                                <span class="mdl-list__item-sub-title"> \
+                                    ' + subquestions[i].question + ' \
+                                </span> \
+                                </span> \
+                            </li>';
+    }
+    questionsList.innerHTML = subQuestionsListString;
+
+    let buttonString = '<button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect"\
+                        onclick="addQuestionsList()"> \
+                        Back \
+                        </button>'
+    questionsList.innerHTML += buttonString;
 }
 
 window.onload = function () {
