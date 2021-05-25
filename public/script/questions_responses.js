3ef296c4aadd946c93d454304dae3564ef0f2637
Dan <dyue0003@student.monash.edu>
2021-05-25 17:16:23 +0800

    Added responsive behaviour for questions_responses.html for tablets and phones

--- public/script/questions_responses.js
+++ public/script/questions_responses.js
@@ -1,18 +1,26 @@
 // Initialising variables
 let selected = null;
 let sub = false;
-let responsesList = document.getElementById("responses-list");
+let responsesList = document.getElementById("responses-list-div");
 let questionsList = document.getElementById("questions-list");
 
 // Hard-coded sample questions & responses
-// TODO integrate with firebase
-let responses = ["Response 1", "Response 2", "Response 3"];
+// TODO: integrate with firebase
+let responses = ["Response 1", "Response 2", "Response 3", "Response 1", "Response 2", "Response 3", "Response 1", "Response 2", "Response 3"];
 let questions = [{question_number: "1.1", question: "What is your name?", type: "none"},
                 {question_number: "1.2", question: "Where are you from?", type: "multiple-choice-sub-question"},
+                {question_number: "1.3", question: "What is your first name, last name, and middle name?", type: "none"},
+                {question_number: "1.3", question: "What is your first name, last name, and middle name?", type: "none"},
+                {question_number: "1.3", question: "What is your first name, last name, and middle name?", type: "none"},
+                {question_number: "1.3", question: "What is your first name, last name, and middle name?", type: "none"},
+                {question_number: "1.3", question: "What is your first name, last name, and middle name?", type: "none"},
+                {question_number: "1.3", question: "What is your first name, last name, and middle name?", type: "none"},
+                {question_number: "1.3", question: "What is your first name, last name, and middle name?", type: "none"},
+                {question_number: "1.3", question: "What is your first name, last name, and middle name?", type: "none"},
                 {question_number: "1.3", question: "What is your first name, last name, and middle name?", type: "none"}];
-let subquestions = [{question_number: "1.2.1", question: "Subquestion: What is your name?", type: "none"},
-                    {question_number: "1.2.2", question: "Subquestion: Where are you from?", type: "none"}, 
-                    {question_number: "1.2.3", question: "Subquestion: What is your first name, last name, and middle name?", type: "none"}];
+let subquestions = [{question_number: "1.2.1", question: "What is your name?", type: "none"},
+                    {question_number: "1.2.2", question: "Where are you from?", type: "none"}, 
+                    {question_number: "1.2.3", question: "What is your first name, last name, and middle name?", type: "none"}];
 
 /**
  * Adds list of questions in questions to the UI
@@ -25,15 +33,16 @@ function addQuestionsList() {
         if (questions[i].type == "multiple-choice-sub-question") {
             innerFunction = "changeSubQuestion(" + i + ")";
         }
-        questionsListString += '<li class="mdl-list__item mdl-list__item--two-line" \
-                    onclick="' + innerFunction + '" id="q' + i + '" style="cursor: pointer;"> \
+
+        questionsListString += '<li class="mdl-list__item mdl-list__item" \
+                                onclick="' + innerFunction + '" id="q' + i + '" \
+                                style="cursor: pointer;"> \
                                 <span class="mdl-list__item-primary-content"> \
-                                <span>Question ' + questions[i].question_number + '</span> \
-                                <span class="mdl-list__item-sub-title"> \
-                                    ' + questions[i].question + ' \
+                                <span>Question ' + questions[i].question_number + ': \
+                                ' + questions[i].question + '</span> \
                                 </span> \
                                 </span> \
-                            </li>';
+                                </li>';
     }
     questionsList.innerHTML = questionsListString;
 }
@@ -45,13 +54,13 @@ function addQuestionsList() {
  */
 function changeQuestion(index) {
     if (selected != null) {
-        selected.style.opacity = 1;
+        selected.style.backgroundColor = "";
     }
 
     let q = "q" + index;
 
     selected = document.getElementById(q)
-    selected.style.opacity = 0.5;
+    selected.style.backgroundColor = "#d8d8d8";
 
     let header = "";
     if (!sub) {
@@ -60,45 +69,74 @@ function changeQuestion(index) {
     else {
         header = "<h3>Question " + subquestions[index].question_number + " Responses</h3>"
     }
-    responsesList.innerHTML = header;
 
-    let responsesListString = "";
-    responsesListString += '<div id="responses-list-div"> \
-                                <ul class="mdl-list">';
+    let list = null;
+    let buttonString = "";
+
+    // If on phone, set responsesList = questionsList
+    if($(window).width() <= 479){
+        list = questionsList;
+        buttonString = '<div style="text-align: center;"> \
+                            <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect"\
+                            onclick="addQuestionsList()" style="width: 45%; margin: auto;"> \
+                            Back \
+                            </button></div>'
+    }
+    // Else, use responsesList
+    else {
+        list = responsesList;
+    }
+    list.innerHTML = header;
+    
+    // TODO: integrate with firebase
+    let listString = "";
+    listString += '<div> \
+                    <ul class="mdl-list id="responses-list">';
     for (let i = 0; i < responses.length; i++) {
-        responsesListString += '<li class="mdl-list__item mdl-list__item"> \
+        listString += '<li class="mdl-list__item mdl-list__item"> \
                                 <span class="mdl-list__item-primary-content"> \
-                                    <span>' + responses[i] + '</span> \
+                                <span>' + responses[i] + '</span> \
                                 </span> \
                                 </li>';
     }
-    responsesList.innerHTML += responsesListString;
+    list.innerHTML += listString +  '<br>';
+    list.innerHTML += buttonString;
 }
 
+/**
+ * Changes the question list to list out the subquestions of a question.
+ */
 function changeSubQuestion() {
     sub = true;
     let subQuestionsListString = "";
     for (let i = 0; i < subquestions.length; i++) {
         let innerFunction = "changeQuestion(" + i + ")";
-        subQuestionsListString += '<li class="mdl-list__item mdl-list__item--two-line" \
-                    onclick="' + innerFunction + '" id="q' + i + '" style="cursor: pointer;"> \
+        subQuestionsListString += '<li class="mdl-list__item mdl-list__item" \
+                                onclick="' + innerFunction + '" id="q' + i + '" \
+                                style="cursor: pointer;"> \
                                 <span class="mdl-list__item-primary-content"> \
-                                <span>Question ' + subquestions[i].question_number + '</span> \
-                                <span class="mdl-list__item-sub-title"> \
-                                    ' + subquestions[i].question + ' \
+                                <span>Question ' + subquestions[i].question_number + ': \
+                                ' + subquestions[i].question + '</span> \
                                 </span> \
-                                </span> \
-                            </li>';
+                                </li>';
     }
-    questionsList.innerHTML = subQuestionsListString;
+    questionsList.innerHTML = subQuestionsListString + '<br>';
 
-    let buttonString = '<button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect"\
-                        onclick="addQuestionsList()"> \
+    let buttonString = '<div style="text-align: center;"> \
+                        <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect"\
+                        onclick="addQuestionsList()" style="width: 45%; margin: auto;"> \
                         Back \
-                        </button>'
+                        </button></div>'
     questionsList.innerHTML += buttonString;
 }
 
 window.onload = function () {
     addQuestionsList();
 };
+
+// Checks everytime the window is resized to prevent two response tabs
+$(window).resize(function() {
+    if($(this).width() > 479 ) {
+        addQuestionsList();
+    }
+});
