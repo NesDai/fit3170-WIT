20231fa790e296c44ddb1bb3d3d4aee4e44c395f
Daiki <dkub0001@student.monash.edu>
2021-08-06 18:03:37 +0900

    added html elements for Video display

--- public/script/forum.js
+++ public/script/forum.js
@@ -178,6 +178,8 @@ function printAllPosts(){
                               <form class="post_content" style="margin:0 10px; background-color: white">
                                  <h6 class="post_content mdl-color-text--black" style="margin:0 10px; background-color: white; padding-left:10px; font-size: 20px" >${post.description} </h6>
                                  <br>
+                                 <iframe width="350" height="300" src="${post.videoURL}"></iframe>   
+                                 <br>
                                  <div style='display: inline-block'>
                                     <button class="mdl-button mdl-js-button  mdl-color-text--white" id="interest1_id">${post.interest[0]} </button>
                                     <button class="mdl-button mdl-js-button mdl-color-text--white" id="interest2_id">${post.interest[1]}</button>
