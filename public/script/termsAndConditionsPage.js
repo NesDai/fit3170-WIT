b4871580796eaed73c4d45eecb61b2a49d49f699
kkon0010 <kkon0010@student.monash.edu>
2021-08-07 16:15:00 +0800

    Created Terms and conditions uploader for the english version of the terms and conditions.
    Changed some css in the terms and conditions html page to make the terms and conditions more readable.
    
    Terms and Conditions.js now can display the terms and conditions onto the page.

--- public/script/termsAndConditionsPage.js
+++ public/script/termsAndConditionsPage.js
@@ -1,6 +1,9 @@
+import { uploadTermsAndConditions_en } from "./termsAndConditions_en_Uploader.js";
+
 let checkbox = document.getElementById("checkbox")
 let tick_warning = document.getElementById("tick-warning")
 let tick_line = document.getElementById("tick-warning-line")
+let content_box = document.getElementById("content-box")
 
 function checkAccepted(){
     if (checkbox.checked){
@@ -10,3 +13,25 @@ function checkAccepted(){
         tick_line.hidden = false
     }
 }
+
+let TermsAndConditionRef = firebase.firestore().collection("Terms&Conditions").doc("T&C_en");
+let content;
+
+TermsAndConditionRef.get().then((doc) => {
+    if (doc.exists) {
+        content =  doc.data();
+        content_box.innerHTML = content.contents;
+    } else {
+        // doc.data() will be undefined in this case
+        uploadTermsAndConditions_en();
+        TermsAndConditionRef.get().then((doc) => {
+            content =  doc.data();
+            content_box.innerHTML = content.contents;
+        });
+    }
+}).catch((error) => {
+    console.log("Error getting document:", error);
+});
+
+content_box.innerHTML = content.contents;
+console.log("content is " + typeof content.contents);
