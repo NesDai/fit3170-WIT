3b5635d797129308536da4d23b9162193da85cd8
kkon0010 <kkon0010@student.monash.edu>
2021-08-13 19:10:21 +0800

    Completed Terms and Conditions Page. Now main page can check if the user already accepted the terms and conditions. Terms and Conditions page now update user's data in realtime database with their acceptance of the terms and conditions.

--- public/script/main_page.js
+++ public/script/main_page.js
@@ -20,14 +20,28 @@ let current_us = JSON.parse(localStorage.getItem("USER"));
 document.getElementById("username").innerHTML = "Welcome @" + current_us["displayName"];
 
 // Checks if user has accepted the terms and conditions already
-function checkUserTAndC(){
-    if (current_us.hasOwnProperty("TermsAndConditionsAccepted")){
-        if (current_us.TermsAndConditionsAccepted == true){
-            window.location.href = "chatbot.html";
-        } else {
-            window.location.href = "termsAndConditionsPage.html";
+function checkUserTAndC() {
+    // initialise phone
+    let phone = current_us.phone
+
+    // check if user's phone number is in database
+    firebase.database().ref(`users/${phone}`).once("value", snapshot => {
+        if (snapshot.exists()){
+            firebase.database().ref(`users/${phone}/termsAndConditionsAccepted`).once("value", snapshot => {
+                // check if termsAndConditionsAccepted field exists
+                if (snapshot.exists()) {
+                    // check if termsAndConditionsAccepted is true
+                    if (snapshot.val()) {
+                        window.location.href = "chatbot.html";
+                    } else {
+                        // if it is not true, move to terms and conditions page
+                        window.location.href = "termsAndConditionsPage.html"
+                    }
+                } else {
+                    // else move user to terms and condition page
+                    window.location.href = "termsAndConditionsPage.html"
+                }
+            })
         }
-    } else {
-        window.location.href = "termsAndConditionsPage.html";
-    }
+    })
 }
--- public/script/termsAndConditionsPage.js
+++ public/script/termsAndConditionsPage.js
@@ -1,22 +1,61 @@
+// import uploadTermsAndConditions_en() function from termsAndConditions_en_Uploader.js
 import { uploadTermsAndConditions_en } from "./termsAndConditions_en_Uploader.js";
 
+//References
 let checkbox = document.getElementById("checkbox")
 let tick_warning = document.getElementById("tick-warning")
 let tick_line = document.getElementById("tick-warning-line")
 let content_box = document.getElementById("content-box")
+let confirmButtonRef = document.getElementById("confirmButton")
 
+// add event listener for when confirm button is clicked
+confirmButtonRef.addEventListener("click", checkAccepted);
+
+// function to check if user has clicked the checkbox before letting them continue to chatbot page
 function checkAccepted(){
+    // if it is checked, update user's realtime database data that they accept the terms and condition
     if (checkbox.checked){
-        window.location.href = "./chatbot.html"
+        // initialise username and phone
+        let username
+        let phone = current_us.phone
+
+        // check if user's phone number exist in database
+        firebase.database().ref(`users/${phone}`).once("value", snapshot => {
+            if (snapshot.exists()){
+                // get user's username from database and asign it to username
+                username = snapshot.val().username;
+
+                //update user's data in database with termsAndConditionsAccepted as true
+                let updates = {};
+                updates['/termsAndConditionsAccepted'] = true;
+
+                firebase.database().ref(`users/${phone}`).update(updates);
+            }
+        })
+
+        //check if termsAndConditionsAccepted is there before sending them to chatbot page
+        firebase.database().ref(`users/${phone}`).once("value", snapshot => {
+            if (snapshot.exists()){
+                if (snapshot.val().termsAndConditionsAccepted) {
+                    window.location.href = "./chatbot.html"
+                }
+            } else {
+                console.log("Terms and Condition Accepted field not updated on user's info in database")
+            }
+        })
+
     } else {
+        // if the tickbox is not ticked, display error message
         tick_warning.hidden = false
         tick_line.hidden = false
     }
 }
 
+// initialise firestore reference to terms and conditions and content
 let TermsAndConditionRef = firebase.firestore().collection("Terms&Conditions").doc("T&C_en");
 let content;
 
+// checks if the collection exists then displays T&C else upload the T&C and then display it.
 TermsAndConditionRef.get().then((doc) => {
     if (doc.exists) {
         content =  doc.data();
@@ -32,6 +71,3 @@ TermsAndConditionRef.get().then((doc) => {
 }).catch((error) => {
     console.log("Error getting document:", error);
 });
-
-content_box.innerHTML = content.contents;
-console.log("content is " + typeof content.contents);
