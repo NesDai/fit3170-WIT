71cbdbea1caf1fec110caeed4148d3fec93392da
kkon0010 <kkon0010@student.monash.edu>
2021-09-02 22:01:12 +0800

    chatbot card will now direct user to termsAndConditionsPage.html (bug fix)

--- public/script/termsAndConditionsPage.js
+++ public/script/termsAndConditionsPage.js
@@ -38,23 +38,25 @@ function checkAccepted(){
     }
 }
 
-// initialise firestore reference to terms and conditions and content
-let TermsAndConditionRef = firebase.firestore().collection("Terms&Conditions").doc("T&C_en_short");
-let content;
+window.onload = function(){
+    // initialise firestore reference to terms and conditions and content
+    let TermsAndConditionRef = firebase.firestore().collection("Terms&Conditions").doc("T&C_en_short");
+    let content;
 
-// checks if the collection exists then displays T&C else upload the T&C and then display it.
-TermsAndConditionRef.get().then((doc) => {
-    if (doc.exists) {
-        content =  doc.data();
-        content_box.innerHTML = content.contents;
-    } else {
-        // doc.data() will be undefined in this case
-        uploadTermsAndConditions_en_short();
-        TermsAndConditionRef.get().then((doc) => {
+    // checks if the collection exists then displays T&C else upload the T&C and then display it.
+    TermsAndConditionRef.get().then((doc) => {
+        if (doc.exists) {
             content =  doc.data();
             content_box.innerHTML = content.contents;
-        });
-    }
-}).catch((error) => {
-    console.log("Error getting document:", error);
-});
+        } else {
+            // doc.data() will be undefined in this case
+            uploadTermsAndConditions_en_short();
+            TermsAndConditionRef.get().then((doc) => {
+                content =  doc.data();
+                content_box.innerHTML = content.contents;
+            });
+        }
+    }).catch((error) => {
+        console.log("Error getting document:", error);
+    });
+}
