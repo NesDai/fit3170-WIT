c6ff864850adad0c21a9d55578f1a7d5a8c9644e
vlad <goroy007@gmail.com>
2021-09-19 10:56:25 +0800

    added comment for the admin login function

--- public/dashboard/script/login.js
+++ public/dashboard/script/login.js
@@ -1,5 +1,11 @@
 const EMAILS = ["goroy007@gmail.com"]; // the preset emails of valid admins
 
+
+/**
+ * the function is used to check if the user's entered email is valid or not, and perform the according actions.
+ * Redirect to admin dashboard if correct email, and display an error otherwise
+ * @param {1} email the input input from the user 
+ */
 function adminLogin(email){
 
     if(EMAILS.includes(email))
