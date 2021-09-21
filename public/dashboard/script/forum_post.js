2f9cb7ce509bab0439f60fadf5135151b05b223d
vlad <goroy007@gmail.com>
2021-09-21 11:21:03 +0800

    added details table for post page

--- public/dashboard/script/forum_post.js
+++ public/dashboard/script/forum_post.js
@@ -15,7 +15,7 @@ async function execute(){
     // onload function
     collectPosts().then(()=>{
         
-        updateUI();
+        updateUI(null);
         // autocomplete(document.getElementById("searchInput"), postid);
 
 
@@ -58,12 +58,91 @@ async function collectPosts(){
 /**
  * Function used to update all the data on the user interface
  */
-function updateUI(){
+function updateUI(postId){
+
+  
 
     $("#postsByUsers").html(`<h3>${numOfPostsUsers}</h3>`);
     $("#postsByRecommender").html(`<h3>${numOfPostsRecommender}</h3>`);
 
+    if(postId==null){
+        return // exit
+    }
+
+    let post;
+
+    for (let i =0; i<posts.length ;i++){
+        if(posts[i].id == postId){
+            post = posts[i]
+            break; //break out of the loop when found the unique key
+        }
+    }
+
+    //else continue to show data
+    $("#chart").html(`
+            <div class="spinner-border" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+    `);
+    $("#postDetailTable").html(`
+            <div class="spinner-border" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+    `);
+
+
+
+    let creator;
+    let interests;
+
+    if(post.creator != undefined){
+        creator = post.creator;
+    }
+    else{
+        creator = "Recommender";
+    }
+
+    
+    if(post.interest.length == 2){
+        interests = post.interest[0] + ", " + post.interest[1]
+    }
+    else{
+        interests = post.interest[0]
+    }
+
+    $("#postDetailTable").html(`
+                <table class="table table-bordered">
+                <tr>
+                    <th>POST TITLE</th>
+                    <td>${post.title}</td>
+                </tr>
+
+                <tr>
+                    <th>DESCRIPTION</th>
+                    <td>${post.description} </td>
+                </tr>
+
+                <tr>
+                    <th>VIDEO LINK</th>
+                    <td>${post.videoURL}</td>
+                </tr>
+
+                <tr>
+                    <th>CREATOR or RECOMMENDED</th>
+                    <td>${creator}</td>
+                </tr>
+
+                <tr>
+                    <th>APPLIED INTERESTS</th>
+                    <td>${interests}</td>
+                </tr>
+
+            </table>
+    `);
+
 }
+
+
 
 
 // update posts on an interval (10 sec) to mimic realtime dashboard
