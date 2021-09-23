7a4e510c6f71f666bdc69d15e2e7258d39454d72
Daiki <dkub0001@student.monash.edu>
2021-09-24 01:21:22 +0900

    csv export in progress

--- public/dashboard/script/forum_csv_export.js
+++ public/dashboard/script/forum_csv_export.js
@@ -1,8 +1,25 @@
 
+function objectToCsv(data) {
 
-function csvExportForum() {
-    firebase.database().ref().once("value").then(snapshot => {
-       root_data = snapshot.val();
-       console.log(root_data);
+    const csv_rows = [];
+    // get the headers 
+    const headers = Object.keys(data[0]);
+    csv_rows.push(headers.join(','));
+    console.log(headers);
+}
+
+function csvExportForum() { // later change the function name appropriately
+    firebase.database().ref('users').once("value").then(snapshot => {
+      user_data = snapshot.val(); 
     });
+    console.log(user_data);
+    console.log("outside");
+
+    const data = user_data.map(user => ({
+        user_id: a,
+        username: user.username, 
+        phone: user.phone,
+    }));
+
+    const csvData = objectToCsv(data);
 }
