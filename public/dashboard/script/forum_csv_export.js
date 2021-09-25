e82503e6d6468184458165e87753ed7b5a0b1c4d
Daiki <dkub0001@student.monash.edu>
2021-09-25 21:00:37 +0900

    made it able to download user data in csv format

--- public/dashboard/script/forum_csv_export.js
+++ public/dashboard/script/forum_csv_export.js
@@ -1,26 +1,40 @@
 
 function objectToCsv(data) {
 
-    const csv_rows = [];
     // get the headers 
-    const headers = Object.keys(data[0]);
-    csv_rows.push(headers.join(','));
-    //console.log(headers);
+    var headers = Object.keys(data[0]);
+    headers = headers.join(',') + "\n";
 
     // loop through the rows
-    for (const row of data) {
-        const values = headers.map(header => {
-            const escaped = (''+row[header]).replace(/"/g, '\\"');
-            return `"${escaped}"`;
-        });
-        csv_rows.push(values.join(','));
-    }
-    console.log(csv_rows);
+    var body = data.map(function(d) {
+        return Object.keys(d).map(function(key) {
+            return d[key];
+        }).join(',');
+    }).join("\n");
+
+    return headers + body;
+}
+
+function csvDownload(csv_data) {
+
+    // output filename 
+    var filename = "forum user data" + '.' + "csv";
+
+    var blob = new Blob([csv_data], {type: 'text/csv;charset=utf-8'});
+    const url = window.URL.createObjectURL(blob);
+    const a= document.createElement('a');
+    a.setAttribute('hidden', '');
+    a.setAttribute('href', url);
+    a.setAttribute('download', filename);
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
 }
 
 function csvExportForum() { // later change the function name appropriately
     const user_arr = [];
     var data;
+    var csv_data;
     firebase.database().ref('users').once("value", x => {
         x.forEach(snapshot => {
             user_arr.push(snapshot.val());
@@ -31,6 +45,10 @@ function csvExportForum() { // later change the function name appropriately
             }));
         });
     }).then(() => {
-        const csvData = objectToCsv(data); 
+        csv_data = objectToCsv(data); 
+    }).then(() => {
+        csvDownload(csv_data);
     });
 }
+
+csvExportForum();
