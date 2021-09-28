7010fb2e6941d583730e090b5358e5f248237b5b
Chia Yong Peng <ychi0014@student.monash.edu>
2021-09-28 15:07:02 +0900

    Add files for getUserMedia spike

---
+++ public/chatbot/speech-to-text/script/index.js
@@ -0,0 +1,46 @@
+let mediaRecorder;
+
+function startRecording() {
+    document.getElementById("toggle").innerText = "Stop Recording";
+    document.getElementById("toggle").onclick = stopRecording;
+
+    if (mediaRecorder === undefined) {
+        initMediaRecorder();
+    } else {
+        mediaRecorder.start();
+    }
+}
+
+function stopRecording() {
+    document.getElementById("toggle").innerText = "Start Recording";
+    document.getElementById("toggle").onclick = startRecording;
+
+    mediaRecorder.stop();
+}
+
+function initMediaRecorder() {
+    navigator.mediaDevices
+        .getUserMedia({audio: true, video: false})
+        .then(stream => {
+            handleSuccess(stream);
+        });
+}
+
+function handleSuccess(stream) {
+    const recordedChunks = [];
+    const options = {mimeType: "audio/webm"};
+    mediaRecorder = new MediaRecorder(stream, options);
+
+    mediaRecorder.addEventListener("dataavailable", event => {
+       if (event.data.size > 0){
+           recordedChunks.push(event.data);
+       }
+    });
+
+    mediaRecorder.addEventListener("stop", () => {
+        // When the recording is stopped
+        console.log(new Blob(recordedChunks));
+    });
+
+    mediaRecorder.start();
+}
