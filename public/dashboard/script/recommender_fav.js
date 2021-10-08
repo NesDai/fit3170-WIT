241231e07aff5037ad43a68eacb1873d453c3522
jasmine <jche0155@student.monash.edu>
2021-10-08 09:55:26 +0800

    fixed bar chart data

--- public/dashboard/script/recommender_fav.js
+++ public/dashboard/script/recommender_fav.js
@@ -1,11 +1,24 @@
 // fixed list of skills
-const ict_list = [["Email", 0], ["Online collaboration",0], ["Search for information",0], ["Smartphone/tablet/computer use",0], ["Social media use",0]];
-const social_list = [["Active listening",0], ["Effective communication",0], ["Negotiation skill",0], ["Persuasion",0], ["Relationship management",0]];
-const complementary_list = [["Art",0], ["Caregiving",0], ["Cooking",0], ["Exercise",0], ["Professional writing",0]];
-const work_list = [["Collaboration and teamwork",0], ["Critical thinking",0], ["Entrepreneurship",0], ["People and Leadership",0], ["Personal selling",0]];
+const skill_list = [ ["Email", 0] , ["Online collaboration",0], ["Search for information",0], ["Smartphone/tablet/computer use",0], ["Social media use",0],
+["Active listening",0], ["Effective communication",0], ["Negotiation skill",0], ["Persuasion",0], ["Relationship management",0],
+["Art",0], ["Caregiving",0], ["Cooking",0], ["Exercise",0], ["Professional writing",0], ["Collaboration and teamwork",0], ["Critical thinking",0],
+["Entrepreneurship",0], ["People and Leadership",0], ["Personal selling",0]];
 
-// global variables
-let skill_list;
+window.onload = collectData();
+
+
+/* collect all skill favourite data from firebase */
+function collectData(){
+  let favs = [];
+
+  firebase.database().ref('recommenderData').child('favourite')
+  .once('value', x => {
+      x.forEach(data => {
+          favs.push(data.val());
+          checkSkill(data.val().preferenceType, data.val().favouritedAmmount);
+      })
+  })
+}
 
 /* function to display selected skill */
 function selectedSkill(){
@@ -15,26 +28,8 @@ function selectedSkill(){
   document.getElementById("selected").innerHTML = option.value;
   document.getElementById("selectedBarChart").innerHTML = option.value;
 
-
-
-  // obtain favourites data
-  collectFavourites();
-  // generate bar chart(execute after collectFavourites() completed)
+  // generate bar chart
   updateChart(option.value);
-}
-
-/* function to retrieve favourite data from firebase */
-function collectFavourites(){
-
-    let favs = [];
-
-    firebase.database().ref('recommenderData').child('favourite')
-    .once('value', x => {
-        x.forEach(data => {
-            favs.push(data.val());
-            checkSkill(data.val().preferenceType, data.val().favouritedAmmount);
-        })
-    })
 
 }
 
@@ -48,20 +43,24 @@ function checkSkill(preference, amount){
   }
 }
 
+/* generate bar chart based on selected skill */
 function updateChart(skill) {
 
     // variable
     let xValues = [];
     let yValues = [];
+    let low = 0;
+    let maxi = 0;
 
     // determine which skill is selected;
-    if(skill == "ICT/Tech­nology Skills"){skill_list = ict_list;}
-    else if(skill == "Social Communi­cation Skills"){skill_list = social_list;}
-    else if(skill == "Comple­mentary Skills"){skill_list = complementary_list;}
-    else{skill_list = work_list;}
+    if(skill == "ICT/Tech­nology Skills"){low = 0; maxi = 5;}
+    else if(skill == "Social Communi­cation Skills"){low = 5; maxi = 10;}
+    else if(skill == "Comple­mentary Skills"){low = 10; maxi = 15;}
+    else{skill = low = 15; maxi = 20;}
+
 
     // to get number of post posted for each interest for x-axis and y-axis values
-    for (let i = 0; i < skill_list.length; i++){
+    for (let i = low; i < maxi; i++){
         xValues.push(skill_list[i][0]);
         yValues.push(skill_list[i][1]);
 
