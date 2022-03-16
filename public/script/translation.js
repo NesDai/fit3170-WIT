cb2a0a4fcdff6e19f6ff71ff207ec4cd34fa0e59
Mlee0056 <mlee0056@student.monash.edu>
2022-03-16 15:06:53 +0800

    Hard code home page translation

--- public/script/translation.js
+++ public/script/translation.js
@@ -76,15 +76,84 @@ function getUserLanguage(){
 }
 
 function changeLang(lang){
-    console.log(window.location);
-    eraseCookiesGoogle();
-    translateLanguage(lang);
+     console.log(window.location);
+    // eraseCookiesGoogle();
+    // translateLanguage(lang);
 
     // if (window.location.pathname =="/"){  //reset the captcha
     //     setTimeout(function(){
     //         window.location.reload(false);
     //     }, 1000);
     //     }
+    localStorage.setItem(LANGUAGE_KEY, lang);
+    if(lang == "English"){
+        document.getElementById("username").innerHTML = "Welcome @" + current_us["username"];
+        document.getElementById("translate_btn").innerHTML = "Translate";
+        document.getElementById("chatbox_btn").innerHTML = "Chat Bot";
+
+        document.getElementById("homeBanner").innerHTML = "Home Page";
+        document.getElementById("chooseLang").innerHTML = "Choose Language:";
+        document.getElementById("button-close-modal").innerHTML = "Back";
+        document.getElementById("selectOne").innerHTML = "Choose one of the following:";
+        document.getElementById("chatBoxHeader").innerHTML = "Chat Bot";
+        document.getElementById("chatBoxText").innerHTML = "Answer simple questions and get the recommendations according to your interests";
+        document.getElementById("chatBoxLink").innerHTML = "Open Chat bot";
+        document.getElementById("recommendHeader").innerHTML = "Recommender";
+        document.getElementById("recommendText").innerHTML = "Get recommendations of the videos and articles that suit your interests";
+        document.getElementById("forumHeader").innerHTML = "Forum";
+        document.getElementById("forumText").innerHTML = "Interact with other users by liking and commenting their posts and post your own thoughts and questions";
+    }
+    else if(lang == "Malay"){
+        document.getElementById("username").innerHTML = "selamat datang @" + current_us["username"];
+        document.getElementById("translate_btn").innerHTML = "terjemah";
+        document.getElementById("chatbox_btn").innerHTML = "Bot Sembang";
+
+        document.getElementById("homeBanner").innerHTML = "Halaman Utama";
+        document.getElementById("chooseLang").innerHTML = "Pilih Bahasa:";
+        document.getElementById("button-close-modal").innerHTML = "Belakang";
+        document.getElementById("selectOne").innerHTML = "Pilih salah satu daripada yang berikut:";
+        document.getElementById("chatBoxHeader").innerHTML = "Bot Sembang";
+        document.getElementById("chatBoxText").innerHTML = "Jawab soalan mudah dan dapatkan cadangan mengikut minat anda";
+        document.getElementById("chatBoxLink").innerHTML = "BUKA BOT SEMBANG";
+        document.getElementById("recommendHeader").innerHTML = "Pengesyor";
+        document.getElementById("recommendText").innerHTML = "Dapatkan cadangan video dan artikel yang sesuai dengan minat anda";
+        document.getElementById("forumHeader").innerHTML = "Forum";
+        document.getElementById("forumText").innerHTML = "Berinteraksi dengan pengguna lain dengan menyukai dan mengulas siaran mereka dan menyiarkan pemikiran dan soalan anda sendiri";
+    }
+    else if(lang == "Chinese (Simplified)"){
+        document.getElementById("username").innerHTML = "欢迎 @" + current_us["username"];
+        document.getElementById("translate_btn").innerHTML = "翻译";
+        document.getElementById("chatbox_btn").innerHTML = "聊天机器人";
+
+        document.getElementById("homeBanner").innerHTML = "主页";
+        document.getElementById("chooseLang").innerHTML = "选择语言：";
+        document.getElementById("button-close-modal").innerHTML = "后面";
+        document.getElementById("selectOne").innerHTML = "选择以下选项之一：";
+        document.getElementById("chatBoxHeader").innerHTML = "聊天机器人";
+        document.getElementById("chatBoxText").innerHTML = "回答简单的问题并根据您的兴趣获得建议";
+        document.getElementById("chatBoxLink").innerHTML = "打开聊天机器人";
+        document.getElementById("recommendHeader").innerHTML = "推荐人";
+        document.getElementById("recommendText").innerHTML = "获取适合您兴趣的视频和文章的推荐";
+        document.getElementById("forumHeader").innerHTML = "论坛";
+        document.getElementById("forumText").innerHTML = "通过喜欢和评论他们的帖子并发布您自己的想法和问题与其他用户互动";
+    }
+    else{
+        document.getElementById("username").innerHTML = "ยินดีต้อนรับ @" + current_us["username"];
+        document.getElementById("translate_btn").innerHTML = "แปลภาษา";
+        document.getElementById("chatbox_btn").innerHTML = "แชทบอท";
+
+        document.getElementById("homeBanner").innerHTML = "หน้าแรก";
+        document.getElementById("chooseLang").innerHTML = "เลือกภาษา:";
+        document.getElementById("button-close-modal").innerHTML = "กลับ";
+        document.getElementById("selectOne").innerHTML = "เลือกอย่างใดอย่างหนึ่งต่อไปนี้:";
+        document.getElementById("chatBoxHeader").innerHTML = "แชทบอท";
+        document.getElementById("chatBoxText").innerHTML = "ตอบคำถามง่าย ๆ และรับคำแนะนำตามความสนใจของคุณ";
+        document.getElementById("chatBoxLink").innerHTML = "เปิดแชทบอท";
+        document.getElementById("recommendHeader").innerHTML = "ผู้แนะนำ";
+        document.getElementById("recommendText").innerHTML = "รับคำแนะนำของวิดีโอและบทความที่เหมาะกับความสนใจของคุณ";
+        document.getElementById("forumHeader").innerHTML = "ฟอรั่ม";
+        document.getElementById("forumText").innerHTML = "โต้ตอบกับผู้ใช้รายอื่นด้วยการกดไลค์และแสดงความคิดเห็นโพสต์ของพวกเขาและโพสต์ความคิดและคำถามของคุณเอง"; 
+    }
 
 }  
 
@@ -101,7 +170,6 @@ function reset(){
             window.location.reload(false);
         }, 1000);
     }
-
 }
 
 
